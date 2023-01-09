5cb1cd06194f19677df63986ed55ee16038034d8
Siemba Blessed <simbamahla@gmail.com>
2023-01-09 12:07:02 +0200

    mn

---
+++ JS/javascript.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    /* $("#card1").hide(500).show(2000).hide(500).show(2000); */
+    $("#card2").slideUp(1000).slideDown(1000);
+
+
+    $("#panel3").css({
+        color:'purple',
+        fontWeight:'bold',
+        /* display:'none', */
+    });
+
+});
